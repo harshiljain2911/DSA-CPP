66d25370012213b1eaadaafea6ca6e843e047cb5
harshiljain2911 <kotaharshil2906@gmail.com>
2026-01-23 15:45:22 +0530

    Longest Substring without repeating
    yha humne is question mai SLIDING WINDOW approach use kari jisme hum ek set use kr rhe hai aur 2 pointers hai left aur right jisme initialyy dono string ke index 0 pr hai aur uske baad hum for loop jisme right pointer badhega uske andr ek while loop laga rhe hai joh ki yeh check kr rha hai ki jese hi suplicate element aata hai toh hum left pointer ko badhayenge aur set mai se woh saare elemnt hatate jaayenge jab tak left mai hume unique element nhi mil jata for example abcb hai toh isme oehle set mai a add hoga then b then c then uske baad jese hi right pointer b pr aaya aur b already set mai hai toh yehh while loop mai gya jisme ki sirf left pointer badhega toh ab left initialy 0 tha toh pehle left wala element yani a har gya aur fir left ++ hkr 1 ho gya aur left 1 hokr left mai b hai toh woh bhi det mai se remove ho gya aur leeft mai c aa gay ab aur fir check kra toh b nhi tha set mai toh b ko add kr diya toh ab bacha set mai c aur b aur max len joh hai woh bhi humne max function ke andr daali hu hai ki kisi sybstring ki length max aa gyii ho uske baad saati substrigs usse kam length ki ho toh length chali nhi jaaye kahi

---
+++ Day_13/longestsubstringwithoutrepeating.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+{
+
+    int maxlength = 0;
+    set<char> st;
+
+    int left = 0;
+
+    for (int right = 0; right < s.size(); right++)
+    {
+        while (st.find(s[right]) != st.end())
+        {
+            st.erase(s[left]);
+            left++;
+        }
+
+        st.insert(s[right]);
+        maxlength = max(maxlength, right - left + 1);
+    }
+
+    return maxlength;
+}
+};
