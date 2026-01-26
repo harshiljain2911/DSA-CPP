f7a4cd5b7d196e8a00f3af4f4293ac1a6db3a94e
harshiljain2911 <kotaharshil2906@gmail.com>
2026-01-26 11:27:52 +0530

    Fruits into Basket
    yha par bhi humne same sliding window wala concept lgaya hai aur yha hua kya hai ki hum map use kr rhe hai aur jese hi map mai koi 3 type ka fruit add hua toh hum map ko fir i mean window mai left ko ++ kar rhe hai

---
+++ Day_15/fruitsintobasket.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+    int left=0;
+    int right;
+    unordered_map<int,int> m;
+    int maxlen = 0;
+
+    for(right = 0;right<fruits.size();right++)
+    {
+       
+            m[fruits[right]]++;
+                
+            while(m.size()>2)
+            {
+
+                m[fruits[left]]--;
+                if(m[fruits[left]]==0)
+                {
+                    m.erase(fruits[left]);
+                }
+                left++;
+        
+            }
+        
+        maxlen = max(maxlen,right-left+1);
+    }
+
+
+
+
+
+    return maxlen;
+
+
+
+    }
+};
