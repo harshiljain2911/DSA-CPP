e1ff728d933657f5dec056997b67767594abb9bc
Harshil Jain <kotaharshil2906@gmail.com>
2026-01-28 13:41:34 +0530

    Best Optimal Approach Subarray sum k
    yarr yha par humne map ko use kra hai aur humen prefix sum ko use kra hai aur humne yha kra kya hai ki hum kisi particular iteration tak jaa rhe hai aur jab hume K chaiye toh woh equal hoga ki kitni subarray ka sum S-K hai toh hume mtlab hai s-k se aur jese hi hume s-k ka sum milta hai hum use ek map mai store kara dete hai with number of times woh sum aa rha hai mtlam map mai humare pass sum aur kitni subarrays ka sum hai woh mil jata hai aur count mai hum use add kr dete hai aur yeh pura loop hum chlate hai nums.size() tak ke liye aur is se hume final count mil jata hai...

---
+++ Day_17/subarraysumkBest.cpp
@@ -0,0 +1,18 @@
+int subarraySum(vector<int>& nums, int k) {
+        
+    int presum = 0;
+    int count = 0; 
+    map<int,int> mpp;
+    mpp[0]=1;
+
+    for(int i=0;i<nums.size();i++)
+    {
+         presum = presum + nums[i];
+         int remove = presum - k;
+         count = count + mpp[remove];
+         mpp[presum] = mpp[presum]+1;
+    }
+
+    return count;
+
+}
