06a56d9988f54fd454a55d144b672e1b214eb62f
Harshil Jain <kotaharshil2906@gmail.com>
2026-02-03 18:27:11 +0530

    Remove Duplicates II.cpp
    yha par humne kiya kya hai ki hum two pinter approach use kr rhe hai jisse ki hum j aur i ko 2 se start kar rhe hai kyunki atmax 2 duplicates allowed hai aur fir agar nums[i] != nums[j-2] hai matlab duplicate nhi hai toh wha nex valid element store krana hai aur j reprsent kar rha hai next valid position

---
+++ Day_20/remdupII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int removeDuplicates(vector<int>& nums) {
+
+    if(nums.size() <= 2)
+        return nums.size();
+
+    int j = 2;  // next valid position
+
+    for(int i = 2; i < nums.size(); i++)
+    {
+        if(nums[i] != nums[j-2])
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    return j;
+}
+};
