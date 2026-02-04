9d64d75ac122e2a45c5bd5a0f4018f114ad50bb0
Harshil Jain <kotaharshil2906@gmail.com>
2026-02-04 22:24:24 +0530

    Binary Search
    binary search approach hai yarr bass

---
+++ Day_21/BinarySearch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int mid;
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+};
