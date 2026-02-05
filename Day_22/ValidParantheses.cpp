ae513500a39978c1ea96e7ad8a0288603891104e
Harshil Jain <kotaharshil2906@gmail.com>
2026-02-05 11:50:44 +0530

    Valid Parantheses

---
+++ Day_22/ValidParantheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                // closing bracket aaya
+                if (st.empty())
+                    return false;
+
+                char temp = st.top();
+                st.pop();
+
+                if ((s[i] == ')' && temp != '(') ||
+                    (s[i] == '}' && temp != '{') ||
+                    (s[i] == ']' && temp != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
