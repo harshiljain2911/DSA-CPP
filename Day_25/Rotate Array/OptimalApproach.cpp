2bd9f7c50853d1b9ecc4eb676c1749439b6675b0
Harshil Jain <kotaharshil2906@gmail.com>
2026-03-15 17:03:36 +0530

    Optimal Approach
    1→ yeh approach toh bohot hi easy hai mtlhb isme humne built in revers function kas use kiya aur pehle puri array ko reverse kra fir uske baad first k elements ko reverse kra uske baad last k elemnts ko reverse kra

--- Day_25/Rotate Array/OptimalApproach.cpp
+++ Day_25/Rotate Array/OptimalApproach.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
 
         int n = nums.size();
-        k = k % n;
+        k = k % n; // yeh kara hai taki agr k>nums.size() hua toh usko handlke krle as array rotations cycle mai hote hai
 
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
