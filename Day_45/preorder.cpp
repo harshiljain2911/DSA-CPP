7e42d0a9255977ad6b904320825a6ebed450e5cc
Harshil Jain <kotaharshil2906@gmail.com>
2026-06-01 15:40:57 +0530

    Preorder Traversal
    Root-Left-Right

---
+++ Day_45/preorder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> ans;
+
+        if(root == NULL)
+        {
+            return ans;
+        }
+
+        ans.push_back(root->val);
+
+        vector<int> left = preorderTraversal(root->left);
+        vector<int> right = preorderTraversal(root->right);
+
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.insert(ans.end(), right.begin(), right.end());
+
+        return ans;
+    }
+};
