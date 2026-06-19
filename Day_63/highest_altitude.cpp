98a35c65e6773203f430ba3afbcab70f605fd6a1
Harshil Jain <kotaharshil2906@gmail.com>
2026-06-19 11:03:51 +0530

    Find The Highest Altitude

---
+++ Day_63/highest_altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        int sum = 0;
+        int ans = 0;
+        
+        for(int i = 0;i<n;i++)
+        {
+            sum+=gain[i];
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+};
