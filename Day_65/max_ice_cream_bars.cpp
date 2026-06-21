098e5c96527f968723082b3eff0101b6e9d53f72
Harshil Jain <kotaharshil2906@gmail.com>
2026-06-21 14:03:45 +0530

    Maximum Ice Cream Bars

---
+++ Day_65/max_ice_cream_bars.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+
+        int count = 0;
+
+        for(auto& i:costs)
+        {
+            if(coins<i) break;
+            count++;
+            coins-=i;
+        }
+        return count;
+    }
+};
